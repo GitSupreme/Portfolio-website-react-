155199b33d7eaee24e7240613529ee45360128b8
Jephtha Amakye <amakyejephtha@gmail.com>
2025-01-22 19:57:39 +0000

    Completed sidebar component

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const home = () => {
+const Home = () => {
     return(
-        <h1>This is the home page</h1>
+        <p className='homeText'>This is the home page</p>
     )
 }
 
-export default home;
+export default Home;
--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./sidebar.css";
 import Home from '../home/home'; 
+import SidebarItems from "./sidebarItems";
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 
 
-const sidebar = () => {
+const Sidebar = () => {
+    
+    const [expandSideBar, setExpandSidebar] = useState(true);
+
+    const handleExpandClick = () => {
+        setExpandSidebar(!expandSideBar)
+    }
+
     return (
 
         <div className='container-fluid sidebar-section'>
             
-            <div className = 'sidebar'>
+            <div className = {expandSideBar ? 'sidebar':'sidebar-expand'}>
+                
                 <div className='expand-collapse-icon'>
-
+                    <p onClick={handleExpandClick}>
+                        {expandSideBar ? 
+                            (<BsChevronRight size={30}/>) : 
+                            (<BsChevronLeft size={30}/>)
+                        }
+                    </p>
                 </div>
+                <SidebarItems expandSideBar={expandSideBar}/>
             </div>
 
             <div className='container'>
                 <Home/>
             </div>
 
-
         </div>
 
     )
 }
 
-export default sidebar;
+export default Sidebar;
